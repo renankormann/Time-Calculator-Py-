a242454cc76dfb37bb7465696878cd604c79d643
Renan Caleffo Kormann <103371525+renankormann@users.noreply.github.com>
2025-01-09 14:50:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ Created on Jul 12, 2023
 '''
 
 from timeCalc import add_time
-
+# I used this file just to test the function by passing different values to it.
 print(add_time("11:06 PM", "250:02", "Monday"))
